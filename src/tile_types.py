290f9aa29b20e7b30bbec68befe62927be05366d
Starmadeguy <starmadelich@gmail.com>
2025-07-24 10:08:57 -0700

    part 3

--- src/tile_types.py
+++ src/tile_types.py
@@ -32,8 +32,8 @@ def new_tile(
 
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walkable=True, transparent=True, dark=(ord("."), (255, 255, 255), (50, 50, 150)),
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    walkable=False, transparent=False, dark=(ord("#"), (255, 255, 255), (0, 0, 100)),
 )
